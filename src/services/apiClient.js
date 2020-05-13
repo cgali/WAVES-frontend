a917b432d9710e0f7964333d736a3bd7a2ab9369
Carles Galí Bou <carles_ga@hotmail.com>
2020-05-13 12:07:04 +0200

    Added at "apiClient" all the routes of backend

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -8,12 +8,126 @@ class ApiClient {
     });
   }
 
+// AUTH => ROUTES
+
   whoami() {
     return this.apiClient.get("/whoami");
   }
 
   login(body) {
     return this.apiClient.post("/login", body);
+  }
+
+  signUp(body) {
+    return this.apiClient.post("/signup", body);
+  }
+
+  logout() {
+    return this.apiClient.get("/logout");
+  }
+
+// PROFILE => ROUTES
+
+  profile() {
+    return this.apiClient.get("/profile");
+  }
+
+  updateProfile(body) {
+    return this.apiClient.put("/profile", body);
+  }
+
+  deleteProfile() {
+    return this.apiClient.delete("/profile")
+  }
+
+// SURFERS => ROUTES
+
+  surfersList() {
+    return this.apiClient.get("/surfers-list");
+  }
+
+  surferProfile(id) {
+    return this.apiClient.get(`/surfers-list/${id}`);
+  }
+
+// BEACHES => ROUTES
+
+  beachesList() {
+    return this.apiClient.get("/beaches-list")
+  }
+
+  beachProfile(id) {
+    return this.apiClient.get(`/beaches-list/${id}`);
+  }
+
+// BEACHES => REVIEW ROUTES
+
+  createBeachReview(id, body) {
+    return this.apiClient.post(`/beaches-list/${id}/add-review`, body);
+  }
+
+  updateBeachReview(id, _id, body) {
+    return this.apiClient.post(`/beaches-list/${id}/update-review/${_id}`, body);
+  }
+
+  deleteBeachReview(id, _id, body) {
+    return this.apiClient.post(`/beaches-list/${id}/delete-review/${_id}`, body);
+  }
+
+// BEACHES => RATE ROUTES
+
+  createBeachRate(id, body) {
+    return this.apiClient.post(`/beaches-list/${id}/add-rate`, body);
+  }
+
+  updateBeachRate(id, _id, body) {
+    return this.apiClient.post(`/beaches-list/${id}/update-rate/${_id}`, body);
+  }
+
+  deleteBeachRate(id, _id, body) {
+    return this.apiClient.post(`/beaches-list/${id}/delete-rate/${_id}`, body);
+  }
+
+// EVENTS => ROUTES
+
+  eventsList() {
+    return this.apiClient.get("/events-list")
+  }
+
+  eventsProfile(id) {
+    return this.apiClient.get(`/events-list/${id}`);
+  }
+
+  createEvent(body) {
+    return this.apiClient.post(`/events-list`, body);
+  }
+
+  updateEvent(id, body) {
+    return this.apiClient.put(`/events-list/${id}`, body);
+  }
+
+  deleteEvent(id) {
+    return this.apiClient.delete(`/events-list/${id}`);
+  }
+
+// EVENTS => PARTICIPANT ROUTES
+
+  AddRemoveParticipant(id, body) {
+    return this.apiClient.post(`/events-list/${id}/participant`, body);
+  }
+
+// BEACHES => REVIEW ROUTES
+
+  createEventReview(id, body) {
+    return this.apiClient.post(`/events-list/${id}/add-review`, body);
+  }
+
+  updateEventReview(id, _id, body) {
+    return this.apiClient.post(`/events-list/${id}/update-review/${_id}`, body);
+  }
+
+  deleteEventReview(id, _id, body) {
+    return this.apiClient.post(`/events-list/${id}/delete-review/${_id}`, body);
   }
 }
 
---
+++ src/views/surfers/SurfersList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import "../surfers/surfersList.css"
+
+
+class SurfersList extends Component {
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default SurfersList;
